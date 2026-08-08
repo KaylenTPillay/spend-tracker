4236ffdcfdeb06e0b5b783a434532fae235aab7e
Kaylen Travis Pillay <kaylenpillay+code@proton.me>
2026-08-08 20:41:36 +0200

    Initialise repository

---
+++ settings.gradle.kts
@@ -0,0 +1,32 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+
+@Suppress("UnstableApiUsage")
+dependencyResolutionManagement {
+    // The warnings below are due to these features being in Incubation by Gradle.
+    // These feature API may change in the next Gradle release.
+    // If there are any issues, or suggestions for improvements, a report to Gradle
+    // can be made.
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Tracker"
+include(":app")
