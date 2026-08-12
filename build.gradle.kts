cef2cffb849607e89b06cf938fb29894386ad30d
Kaylen Travis Pillay <kaylenpillay+code@proton.me>
2026-08-12 16:54:58 +0200

    feat(lint-setup): add module lint rules setup
    Add module lint rules setup for all modules on the app.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,32 @@
+import com.android.build.api.dsl.ApplicationExtension
+import com.android.build.api.dsl.LibraryExtension
+import com.android.build.api.dsl.Lint
+
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.android.library) apply false
+}
+
+subprojects {
+    plugins.withId("com.android.application") {
+        extensions.configure<ApplicationExtension> {
+            configureAndroidLint(lint)
+        }
+    }
+    plugins.withId("com.android.library") {
+        extensions.configure<LibraryExtension> {
+            configureAndroidLint(lint)
+        }
+    }
+}
+
+fun configureAndroidLint(lint: Lint) {
+    lint.apply {
+        abortOnError = true
+        checkAllWarnings = true
+        warningsAsErrors = false
+    }
 }
