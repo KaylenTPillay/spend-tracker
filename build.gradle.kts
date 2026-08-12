2bcdaa2ff2681e455725645a7a31ed99e3fc5b92
Kaylen Travis Pillay <kaylenpillay+code@proton.me>
2026-08-12 18:26:21 +0200

    feat(lint-setup): add detekt integration and config
    Add initial setup for detekt and config file.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 import com.android.build.api.dsl.ApplicationExtension
 import com.android.build.api.dsl.LibraryExtension
 import com.android.build.api.dsl.Lint
+import dev.detekt.gradle.extensions.DetektExtension
 
 plugins {
     alias(libs.plugins.android.application) apply false
@@ -8,17 +9,25 @@ plugins {
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.detekt) apply false
 }
 
 subprojects {
+    plugins.apply("dev.detekt")
     plugins.withId("com.android.application") {
         extensions.configure<ApplicationExtension> {
             configureAndroidLint(lint)
+        }
+        extensions.configure<DetektExtension> {
+            configureStaticAnalysis()
         }
     }
     plugins.withId("com.android.library") {
         extensions.configure<LibraryExtension> {
             configureAndroidLint(lint)
+        }
+        extensions.configure<DetektExtension> {
+            configureStaticAnalysis()
         }
     }
 }
@@ -29,4 +38,9 @@ fun configureAndroidLint(lint: Lint) {
         checkAllWarnings = true
         warningsAsErrors = false
     }
+}
+
+fun DetektExtension.configureStaticAnalysis() {
+    config.setFrom(file("config/detekt/detekt.yml"))
+    buildUponDefaultConfig = true
 }
