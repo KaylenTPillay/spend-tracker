6c73f05073b3c38a69b6d845046cd53de8d19319
Kaylen Travis Pillay <kaylenpillay+code@proton.me>
2026-08-16 11:13:28 +0200

    test(dashboard): add mapper local source unit test

---
+++ core/data/src/test/java/com/kaylentravispillay/core/data/local/source/model/mapper/MapperLocalSourceTest.kt
@@ -0,0 +1,50 @@
+package com.kaylentravispillay.core.data.local.source.model.mapper
+
+import com.kaylentravispillay.core.data.local.database.tables.TransactionEntity
+import com.kaylentravispillay.core.data.local.source.model.mapper.MapperLocalSource.toEntity
+import com.kaylentravispillay.core.domain.model.Category
+import com.kaylentravispillay.core.domain.model.Transaction
+import com.kaylentravispillay.core.domain.model.TransactionType
+import io.kotest.matchers.equals.shouldEqual
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.EnumSource
+
+class MapperLocalSourceTest {
+    @Nested
+    inner class TransactionToEntityTestSuite {
+        @ParameterizedTest
+        @EnumSource(value = TransactionType::class)
+        fun `GIVEN transaction with various types WHEN toEntity is called THEN return mapped TransactionEntity`(
+            transactionType: TransactionType
+        ) {
+            // Arrange
+            val input = Transaction(
+                id = 1,
+                amount = 100,
+                title = "Test-Transaction-Title",
+                type = transactionType,
+                category = Category(
+                    id = 2,
+                    name = "Test-Category-Name",
+                    iconType = "Test-Category-Icon-Type"
+                ),
+                timestamp = 1_000_000L
+            )
+            val expectedEntity = TransactionEntity(
+                id = 1,
+                timestamp = 1_000_000L,
+                amount = 100,
+                title = "Test-Transaction-Title",
+                type = transactionType,
+                categoryId = 2
+            )
+
+            // Act
+            val actualEntity = input.toEntity()
+
+            // Assert
+            expectedEntity shouldEqual actualEntity
+        }
+    }
+}
